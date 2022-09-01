bf480f450acccf19bb25ab2c5448e9aff5870ef9
Santiago <sadavi@unicesar.edu.co>
2022-09-01 09:56:39 -0500

    add method post

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from crypt import methods
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,6 +14,20 @@ def ping():
 def getProducts():
     # return jsonify(products)
     return jsonify({'products': products})
+
+
+@app.route('/products/<string:product_name>')
+def getProduct (product_name):
+    product_found = [product for product in products if product['name'] == product_name.lower()]
+    if(len(product_found) >= 1):
+       return jsonify({'product':product_found[0]})
+    return jsonify("message", "product not found")
+
+@app.route('/products',methods=['POST'])
+def add_product():
+    print(request.json)
+    return 'received'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port =4000)
