c1edbe323d768b04b0e7d3d604a4dd3dded5fc43
Santiago <sadavi@unicesar.edu.co>
2022-09-01 10:32:15 -0500

    add method PUT

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from itertools import product
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -19,15 +20,32 @@ def getProducts():
 @app.route('/products/<string:product_name>')
 def getProduct (product_name):
     product_found = [product for product in products if product['name'] == product_name.lower()]
-    if(len(product_found) >= 1):
+    if(len(product_found) > 0):
        return jsonify({'product':product_found[0]})
     return jsonify("message", "product not found")
 
 @app.route('/products',methods=['POST'])
 def add_product():
-    print(request.json)
-    return 'received'
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message":"product added successfully", "products": products})
 
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        productsFound[0]['name'] = request.json['name']
+        productsFound[0]['price'] = request.json['price']
+        productsFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            'message': 'Product Updated',
+            'product': productsFound[0]
+        })
+    return jsonify({'message': 'Product Not found'})
 
 if __name__ == '__main__':
     app.run(debug=True, port =4000)
